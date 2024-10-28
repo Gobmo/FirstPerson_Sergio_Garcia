5ba7cb1919d553df11b3fd5d01ea8ba32aebbb5b
Gobmo <sergc8pro@gmail.com>
2024-10-28 17:32:34 +0100

    Agua y Gravedad

--- Assets/Scripts/ThirdPerson.cs
+++ Assets/Scripts/ThirdPerson.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class ThirdPerson : MonoBehaviour
 {
-    [SerializeField] float velocidadMov, tiempoSuavizado, velocidadRotacion;
+    [SerializeField] float velocidadMov, tiempoSuavizado, velocidadRotacion, escalaGravedad, radioDeteccion, alturaSalto;
+    [SerializeField] private Transform pies;
+    [SerializeField] LayerMask queEsSuelo;
     CharacterController cC;
     Vector2 input = Vector2.zero;
     Animator animator;
-    // Start is called before the first frame update
+    private Vector3 movimientoVertical;
+    // Start is called before the first frame update 
     void Start()
     {
         cC = GetComponent<CharacterController>();
@@ -31,15 +34,46 @@ public class ThirdPerson : MonoBehaviour
         {
             animator.SetBool("Walking", false);
         }
+
+        DetectarSuelo();
+        AplicarGravedad();
     }
 
     void Movimiento()
     {
-
         float angulorotacion = Mathf.Atan2(input.x, input.y) * Mathf.Rad2Deg + Camera.main.transform.eulerAngles.y;
         float anguloSuave = Mathf.SmoothDampAngle(transform.eulerAngles.y, angulorotacion, ref velocidadRotacion, tiempoSuavizado);
         transform.eulerAngles = new Vector3(0, anguloSuave, 0);
         Vector3 movimiento = Quaternion.Euler(0, angulorotacion, 0) * Vector3.forward;
         cC.Move(movimiento * velocidadMov * Time.deltaTime);
+    }
+
+    private void AplicarGravedad()
+    {
+        movimientoVertical.y += escalaGravedad * Time.deltaTime;
+        cC.Move(movimientoVertical * Time.deltaTime);
+    }
+
+    private void DetectarSuelo()
+    {
+        Collider[] collsDetectados = Physics.OverlapSphere(pies.transform.position, radioDeteccion, queEsSuelo);
+        if (collsDetectados.Length > 0 )
+        {
+            movimientoVertical.y = 0;
+            Salto();
+        }
+    }
+
+    private void Salto()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            movimientoVertical.y = Mathf.Sqrt(-2 * escalaGravedad * alturaSalto);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(pies.transform.position, radioDeteccion);
     }
 }
